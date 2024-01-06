c54164030bb58e4cae2e962d91ec41b11484af10
fahmihafiz <fahmimoon13@gmail.com>
2024-01-06 16:34:44 +0800

    first commit

---
+++ public/includes/config.php
@@ -0,0 +1,37 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+use Dotenv\Dotenv;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+Dotenv::createUnsafeImmutable(__DIR__ . '/../../')->load();
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+try {
+    //     Online
+    $host = getenv("AWS_HOST");
+    $username = getenv("AWS_USER");
+    $password = getenv("AWS_PASSWORD");
+    $database = getenv("AWS_DB_NAME");
+
+    //     for Windows
+    //$ssl = _DIR_ . "/cacert.pem";
+    //     for MacOS
+        $ssl = "/etc/ssl/cert.pem";
+
+    $con = mysqli_init();
+    mysqli_ssl_set($con, NULL, NULL, $ssl, NULL, NULL);
+
+    $con->real_connect($host, $username, $password, $database);
+
+    if ($con->connect_error) {
+        throw new Exception('Not connected to the database. Error: ' . $con->connect_error);
+    }
+} catch (Exception $e) {
+    echo 'An error occurred: ' . $e->getMessage();
+}
